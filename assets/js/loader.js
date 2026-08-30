6ba9f470a8f12c1c8212177fdb1dc3f5b4c61ad5
TommyHawk97 <19512358+TommyHawk97@users.noreply.github.com>
2026-08-30 16:03:52 +0100

    loader height adjusted veritcally

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -2,13 +2,12 @@ var ContentToggle;
 document.addEventListener('DOMContentLoaded', function() {
     ContentToggle = document.getElementById("content");
     ContentToggle.style.visibility = "hidden";
-});
-
-window.onload = function() {
-    $(window).on("load",function(){
-        setTimeout(() => {
-            ContentToggle.style.visibility = "visible";
-            $(".loader-wrapper").fadeOut("slow");
-        }, 1000);
+        window.onload = function() {
+            $(window).on("load",function(){
+                setTimeout(() => {
+                    $(".loader-wrapper").fadeOut("slow");
+                }, 1000);
+                ContentToggle.style.visibility = "visible";
+            });
+        };
     });
-};
